296d00ec156dbb0ecb85c776e01a39e424aeffd1
TOKUYAMARYUTO <c20018@std.it-college.ac.jp>
2020-08-19 11:03:31 +0900

    スクリプト追加

---
+++ Friends to Protect/Assets/Scripts/Damage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    // コリジョンと衝突した場合の処理
+    void OnCollisionEnter(Collision collision)
+    {
+        // 変数 hit に衝突したオブジェクトを格納
+        var hit = collision.gameObject;
+        // 変数 healthに、変数 hit に格納されたオブジェクトのコンポーネント Health を格納
+        var health = hit.GetComponent<Health>();
+        // 変数 health が空でなければ
+        if (health != null)
+        {
+            // ダメージ処理を実行（体力値-10）
+            //（スクリプト health の関数 TakeDamage を実行）
+            health.TakeDamage(10);
+        }
+        // オブジェクトを削除
+        Destroy(gameObject);
+    }
+}
---
+++ Friends to Protect/Assets/Scripts/Shooter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+	//　弾のプレハブ
+	[SerializeField] private GameObject bullet;
+	//　レンズからのオフセット値
+	[SerializeField] private float offset;
+
+	//　弾を飛ばす間隔時間
+	[SerializeField] private float waitTime = 0.1f;
+	//　経過時間
+	private float elapsedTime = 0f;
+
+	void Update()
+	{
+		elapsedTime += Time.deltaTime;
+		if (elapsedTime < waitTime)
+		{
+			return;
+		}
+		if (Input.GetButton("Fire1"))
+		{
+			//　カメラのレンズの中心を求める
+			var centerOfLens = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Camera.main.nearClipPlane + offset));
+			//　カメラのレンズの中心から弾を飛ばす
+			var bulletObj = Instantiate(bullet, centerOfLens, Quaternion.identity) as GameObject;
+			elapsedTime = 0f;
+		}
+	}
+}
