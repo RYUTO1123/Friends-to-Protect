0e4ee455c21c152e9605774b7553303679f601e4
coma12 <c20011@std.it-college.ac.jp>
2020-08-20 11:03:07 +0900

    素材をそ渡す

---
+++ Friends to Protect/Assets/gemugamen1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class gemugamen1 : MonoBehaviour
+{
+    public void PushGoToMainButton()
+    {
+        //ここで移りたいシーンを指定します。
+        SceneManager.LoadScene("gemugamen");
+    }
+}
---
+++ Friends to Protect/Assets/gemukuria.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class gemukuria : MonoBehaviour
+{
+    public void PushGoToMainButton()
+    {
+        //ここで移りたいシーンを指定します。
+        SceneManager.LoadScene("taitoru");
+    }
+}
